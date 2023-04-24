6cbdaa27c655d1c728b45fa9b4a5008f8af11e17
shivam Chaurasiya <shivam.chaurasiya@bentley.com>
2023-04-24 15:43:52 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@
 import "./App.scss";
 
 import { BrowserAuthorizationClient } from "@itwin/browser-authorization";
-import type { IModelConnection, ScreenViewport } from "@itwin/core-frontend";
+import type { ScreenViewport } from "@itwin/core-frontend";
 import { FitViewTool, IModelApp, StandardViewId } from "@itwin/core-frontend";
 import { FillCentered } from "@itwin/core-react";
 import { ProgressLinear } from "@itwin/itwinui-react";
@@ -33,7 +33,6 @@ import {
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import { history } from "./history";
-import { Visualization } from "./Visualization";
 
 const App: React.FC = () => {
   const [iModelId, setIModelId] = useState(process.env.IMJS_IMODEL_ID);
@@ -143,24 +142,6 @@ const App: React.FC = () => {
     await MeasureTools.startup();
   }, []);
 
-  const onIModelConnected = (_imodel: IModelConnection) => {
-
-    IModelApp.viewManager.onViewOpen.addOnce(async (vp: ScreenViewport) => {
-
-      // const viewStyle: DisplayStyleSettingsProps = {
-      //   viewflags: {
-      //     visEdges: false,
-      //     shadows: true
-      //   }
-      // }
-      // vp.overrideDisplayStyle(viewStyle);
-
-      Visualization.hideHouseExterior(vp);
-
-    })
-  }
-
-
   return (
     <div className="viewer-container">
       {!accessToken && (
@@ -192,8 +173,6 @@ const App: React.FC = () => {
           new MeasureToolsUiItemsProvider(),
 
         ]}
-        onIModelConnected={onIModelConnected}
-
       />
     </div>
   );
